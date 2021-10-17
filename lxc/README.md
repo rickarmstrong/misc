d79e2ef594d59e47cd8f569901afc57fd95c4732
Rick Armstrong <waitingfortheelectrician@gmail.com>
2021-10-17 14:35:59 -0700

    New scripts for working with lxc containers.

---
+++ lxc/README.md
@@ -0,0 +1,1 @@
+Scripts for standing-up new LXC containers.
---
+++ lxc/resources/install_ros.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ROSDISTRO=$1
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt update
+sudo apt install ros-${ROSDISTRO}-desktop-full -y
+echo "source /opt/ros/${ROSDISTRO}/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential -y
+sudo apt install python-rosdep -y
+sudo rosdep init
+rosdep update
