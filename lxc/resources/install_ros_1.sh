a42c4988045e16b0706cdcd6c5604353d1ea107b
Rick Armstrong <waitingfortheelectrician@gmail.com>
2022-01-27 12:01:22 -0800

    Install python3-vcstool.

--- lxc/resources/install_ros_1.sh
+++ lxc/resources/install_ros_1.sh
@@ -6,7 +6,7 @@ sudo apt install -y curl # if you haven't already installed curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
 sudo apt install -y ros-noetic-desktop-full
-sudo apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+sudo apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool python3-vcstool build-essential
 sudo rosdep init
 rosdep update
 
